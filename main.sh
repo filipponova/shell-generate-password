8325bfb145b99957f07c22265e94055511a1365b
Filippo Nova <filipponova@BRSPPM-C02CLHPBMD6M.local>
2021-08-11 21:55:17 -0300

    Revert changes after Github Action validate.

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function generate_password () {
+generate_password () {
 
 read -r -p "Enter the number of characters for your password: " number_characters
 PWD=$(openssl rand -base64 "${number_characters}")
@@ -8,4 +8,4 @@ echo "Hi, the password generate for you is: ${PWD}"
 
 }
 
-generate_password()
+generate_password
